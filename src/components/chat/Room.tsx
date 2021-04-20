8772554cdc027f646ac75afe2253ee58af87d2db
pyo-sh <pygosky@gmail.com>
2021-04-20 10:31:36 +0900

    Room Test

--- src/components/chat/Room.tsx
+++ src/components/chat/Room.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import RoomBox from 'styles/chat/RoomBox';
+import Test from 'components/test';
 
 const Room: React.FC = ({ match }: any) => {
     return (
         <RoomBox>
             {match.params.id}
+            <Test/>
         </RoomBox>
     );
 };
