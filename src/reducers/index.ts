a1a7e8aa87cfb55ce68f2e70566bb1620cccf2c6
pyo-sh <pygosky@gmail.com>
2021-04-21 20:16:12 +0900

    delete Tests

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import test from 'reducers/test';
 import user from 'reducers/user';
 // rootReducer Type
 export type RootState = ReturnType<typeof rootReducer>;
 
 const rootReducer = combineReducers({
     user,
-    test
 });
 
 export default rootReducer;
--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,10 +1,8 @@
 import { all, call } from 'redux-saga/effects';
 import user from 'sagas/user';
-import test from 'sagas/test';
 
 export default function* rootSaga() {
     yield all([
         call(user),
-        call(test),
     ]);
 }
